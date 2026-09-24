6f1d52796fff3b2e938d34fb7a8e488d86697367
williamemilsson <williamemilsson@gmail.com>
2026-09-24 11:02:00 +0200

    Gjorde uppgift 1.1b.

--- uppgift1.py
+++ uppgift1.py
@@ -1,9 +1,8 @@
-"""
-#uppgift a; 
-
 import numpy as np
 import matplotlib.pyplot as plt 
 
+"""
+#uppgift 1.1a; 
 
 t0 = 0
 y0 = 1
@@ -24,9 +23,6 @@ for j in range(N):
     t[j+1] = t[j] + h
     y[j+1] = y[j] + h*f(t[j], y[j])
 
-print(t)
-print(y)
-
 plt.plot(t, y, label="f")
 plt.plot(t, g(t), label="g")
 plt.legend()
@@ -34,4 +30,38 @@ plt.grid()
 plt.show()
 """
 
-#Uppgift b; 
+
+"""
+#Uppgift 1.1b; 
+
+t0 = 0
+y0 = 1
+T = 1.2
+
+f = lambda t, y: 1 + t - y 
+g = lambda t: np.exp(-t) + t
+
+h = 0.1 
+N = 12
+
+t = np.zeros(N+1)
+y = np.zeros(N+1)
+
+t[0] = t0
+y[0] = y0
+
+for j in range(N):
+    t[j+1] = t[j] + h
+    y[j+1] = y[j] + h*f(t[j], y[j])
+
+yh = y[-1]
+
+eh = np.abs(yh - g(T))
+print("eh =", eh)
+
+#Vi får utskriften: 
+#eh = 0.018764675431202527 vilket kan avrundas till 0.0188.
+"""
+
+#Uppgift 1.2a; 
+
